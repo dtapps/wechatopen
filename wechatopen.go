321a6d5b00554ff40d1c7dd24dce5f133cac1688
李光春 <gc@dtapp.net>
2022-06-17 15:42:46 +0800

    - update

--- wechatopen.go
+++ wechatopen.go
@@ -7,59 +7,64 @@ import (
 	"gorm.io/gorm"
 )
 
-// App 微信公众号服务
-type App struct {
-	componentAccessToken   string // 第三方平台 access_token
-	componentVerifyTicket  string // 微信后台推送的 ticket
-	preAuthCode            string // 预授权码
-	authorizerAccessToken  string // 接口调用令牌
-	authorizerRefreshToken string // 刷新令牌
-	authorizerAppid        string // 授权方 appid
-	componentAppId         string // 第三方平台 appid
-	componentAppSecret     string // 第三方平台 app_secret
-	messageToken           string
-	messageKey             string
-	redis                  *dorm.RedisClient // 缓存数据库
-	pgsql                  *gorm.DB          // pgsql数据库
-	client                 *gorequest.App    // 请求客户端
-	log                    *golog.Api        // 日志服务
-	logTableName           string            // 日志表名
-	logStatus              bool              // 日志状态
+type ConfigClient struct {
+	ComponentAccessToken   string // 第三方平台 access_token
+	ComponentVerifyTicket  string // 微信后台推送的 ticket
+	PreAuthCode            string // 预授权码
+	AuthorizerAccessToken  string // 接口调用令牌
+	AuthorizerRefreshToken string // 刷新令牌
+	AuthorizerAppid        string // 授权方 appid
+	ComponentAppId         string // 第三方平台 appid
+	ComponentAppSecret     string // 第三方平台 app_secret
+	MessageToken           string
+	MessageKey             string
+	RedisClient            *dorm.RedisClient // 缓存数据库
+	PgsqlDb                *gorm.DB          // pgsql数据库
 }
 
-// NewApp 实例化
-func NewApp(componentAppId string, componentAppSecret string, messageToken string, messageKey string, redis *dorm.RedisClient, pgsql *gorm.DB) *App {
-	app := &App{componentAppId: componentAppId, componentAppSecret: componentAppSecret, messageToken: messageToken, messageKey: messageKey, redis: redis}
-	app.client = gorequest.NewHttp()
-	if pgsql != nil {
-		app.pgsql = pgsql
-		app.logStatus = true
-		app.logTableName = "wechatopen"
-		app.log = golog.NewApi(&golog.ApiConfig{
-			Db:        pgsql,
-			TableName: app.logTableName,
+// Client 微信公众号服务
+type Client struct {
+	client       *gorequest.App // 请求客户端
+	log          *golog.Api     // 日志服务
+	logTableName string         // 日志表名
+	logStatus    bool           // 日志状态
+	config       *ConfigClient  // 配置
+}
+
+func NewClient(config *ConfigClient) *Client {
+
+	c := &Client{config: config}
+
+	c.client = gorequest.NewHttp()
+	if c.config.PgsqlDb != nil {
+		c.logStatus = true
+		c.logTableName = "wechatopen"
+		c.log = golog.NewApi(&golog.ApiConfig{
+			Db:        c.config.PgsqlDb,
+			TableName: c.logTableName,
 		})
 	}
-	return app
+
+	return c
 }
 
-// Config 配置
-func (app *App) Config(componentAppId, componentAppSecret string) *App {
-	app.componentAppId = componentAppId
-	app.componentAppSecret = componentAppSecret
-	return app
+// ConfigComponent 配置
+func (c *Client) ConfigComponent(componentAppId, componentAppSecret string) *Client {
+	c.config.ComponentAppId = componentAppId
+	c.config.ComponentAppSecret = componentAppSecret
+	return c
 }
 
 // ConfigAuthorizer 配置第三方
-func (app *App) ConfigAuthorizer(authorizerAppid string) *App {
-	app.authorizerAppid = authorizerAppid
-	return app
+func (c *Client) ConfigAuthorizer(authorizerAppid string) *Client {
+	c.config.AuthorizerAppid = authorizerAppid
+	return c
 }
 
-func (app *App) request(url string, params map[string]interface{}, method string) (resp gorequest.Response, err error) {
+func (c *Client) request(url string, params map[string]interface{}, method string) (resp gorequest.Response, err error) {
 
 	// 创建请求
-	client := app.client
+	client := c.client
 
 	// 设置请求地址
 	client.SetUri(url)
@@ -80,8 +85,8 @@ func (app *App) request(url string, params map[string]interface{}, method string
 	}
 
 	// 日志
-	if app.logStatus == true {
-		go app.postgresqlLog(request)
+	if c.logStatus == true {
+		go c.postgresqlLog(request)
 	}
 
 	return request, err
