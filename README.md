ff1e4f9b8f9d138418ab276df0d88af37540922c
李光春 <gc@dtapp.net>
2022-07-30 13:45:07 +0800

    - update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h1>
-<a href="https://www.dtapp.net/">Golang WeChatOpen</a>
+<a href="https://www.dtapp.net/">Golang</a>
 </h1>
 
 📦 Golang WeChatOpen
@@ -8,18 +8,10 @@
 [![godoc](https://pkg.go.dev/badge/go.dtapp.net/wechatopen?status.svg)](https://pkg.go.dev/go.dtapp.net/wechatopen)
 [![goproxy.cn](https://goproxy.cn/stats/go.dtapp.net/wechatopen/badges/download-count.svg)](https://goproxy.cn/stats/go.dtapp.net/wechatopen)
 [![goreportcard.com](https://goreportcard.com/badge/go.dtapp.net/wechatopen)](https://goreportcard.com/report/go.dtapp.net/wechatopen)
-[![deps.dev](https://img.shields.io/badge/deps-go-red.svg)](https://deps.dev/go/go.dtapp.net/wechatopen)
+[![deps.dev](https://img.shields.io/badge/deps-go-red.svg)](https://deps.dev/go/go.dtapp.net%2Fwechatopen)
 
-#### 安装使用
+#### 安装
 
 ```go
 go get -v -u go.dtapp.net/wechatopen
-```
-
-#### 导入
-
-```go
-import (
-    "go.dtapp.net/wechatopen"
-)
 ```
---
+++ client.go
@@ -0,0 +1,75 @@
+package wechatopen
+
+import (
+	"go.dtapp.net/dorm"
+	"go.dtapp.net/golog"
+	"go.dtapp.net/gorequest"
+	"gorm.io/gorm"
+)
+
+type ConfigClient struct {
+	ComponentAccessToken   string // 第三方平台 access_token
+	ComponentVerifyTicket  string // 微信后台推送的 ticket
+	PreAuthCode            string // 预授权码
+	AuthorizerAccessToken  string // 接口调用令牌
+	AuthorizerRefreshToken string // 刷新令牌
+	AuthorizerAppid        string // 授权方 appid
+	ComponentAppId         string // 第三方平台 appid
+	ComponentAppSecret     string // 第三方平台 app_secret
+	MessageToken           string
+	MessageKey             string
+	RedisClient            *dorm.RedisClient // 缓存数据库
+	MongoDb                *dorm.MongoClient // 日志数据库
+	PgsqlDb                *gorm.DB          // 日志数据库
+	DatabaseName           string            // 库名
+}
+
+// Client 微信公众号服务
+type Client struct {
+	client *gorequest.App   // 请求客户端
+	log    *golog.ApiClient // 日志服务
+	config *ConfigClient    // 配置
+}
+
+func NewClient(config *ConfigClient) (*Client, error) {
+
+	var err error
+	c := &Client{config: config}
+
+	c.client = gorequest.NewHttp()
+
+	if c.config.PgsqlDb != nil {
+		c.log, err = golog.NewApiClient(
+			golog.WithGormClient(c.config.PgsqlDb),
+			golog.WithTableName(logTable),
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if c.config.MongoDb != nil {
+		c.log, err = golog.NewApiClient(
+			golog.WithMongoClient(c.config.MongoDb),
+			golog.WithDatabaseName(c.config.DatabaseName),
+			golog.WithCollectionName(logTable),
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return c, nil
+}
+
+// ConfigComponent 配置
+func (c *Client) ConfigComponent(componentAppId, componentAppSecret string) *Client {
+	c.config.ComponentAppId = componentAppId
+	c.config.ComponentAppSecret = componentAppSecret
+	return c
+}
+
+// ConfigAuthorizer 配置第三方
+func (c *Client) ConfigAuthorizer(authorizerAppid string) *Client {
+	c.config.AuthorizerAppid = authorizerAppid
+	return c
+}
